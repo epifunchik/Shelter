8732d0bb85fcd7433d571ec1eae3a31e5b7b3e5f
Olga Ausianik <volha.ausianik@yandex.ru>
2023-04-12 12:42:16 +0300

    feat: redact menu burger

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -7,21 +7,60 @@ const navLink = document.querySelectorAll('.nav-link');
 const navLinkPage = document.querySelector('.nav-link-about');
 const navBurger = document.querySelector('.nav-burger');
 const link = document.querySelector('.link');
+const wrapper = document.querySelector('.wrapper')
 
 menu.addEventListener('click', function() {
-    menu.classList.toggle('active')
+    menu.classList.toggle('active');
+    wrapper.classList.toggle('active');
     span.classList.toggle('active');
     navigation.classList.toggle('active');
     navLinkPage.classList.toggle('active');
-    navBurger.classList.toggle('active');
     link.classList.toggle('active');
     navLink[0].classList.toggle('active');
     navLink[1].classList.toggle('active');
     navLink[2].classList.toggle('active');
+    if (navBurger.classList.contains('hidden')){
+        navBurger.classList.remove('hidden');
+    navBurger.classList.toggle('active');
+    }
+    // navBurger.classList.toggle('hidden');
+    navBurger.classList.toggle('active');
 })
-// navigation.addEventListener('click', function() {
-//     menu.classList.toggle('active');
-//     span.classList.toggle('active');
-//     navigation.classList.toggle('hidden');
-//     navBurger.classList.toggle('hidden');
-// })
+
+navigation.addEventListener('click', function() {
+    menu.classList.toggle('active')
+    span.classList.toggle('active');
+    navigation.classList.toggle('active');
+    navBurger.classList.add('hidden');
+    navLinkPage.classList.toggle('active');
+    link.classList.toggle('active');
+    navLink[0].classList.toggle('active');
+    navLink[1].classList.toggle('active');
+    navLink[2].classList.toggle('active');
+    wrapper.classList.toggle('active');
+})
+wrapper.addEventListener('click', function() {
+    menu.classList.toggle('active')
+    span.classList.toggle('active');
+    navigation.classList.toggle('active');
+    navBurger.classList.add('hidden');
+    navLinkPage.classList.toggle('active');
+    link.classList.toggle('active');
+    navLink[0].classList.toggle('active');
+    navLink[1].classList.toggle('active');
+    navLink[2].classList.toggle('active');
+    wrapper.classList.toggle('active');
+})
+
+// if(document.documentElement.clientWidth > 767){
+//     menu.classList.remove('active')
+//     span.classList.remove('active');
+//     navigation.classList.remove('active');
+//     navBurger.classList.remove('active');
+//     navLinkPage.classList.remove('active');
+//     link.classList.remove('active');
+//     navLink[0].classList.remove('active');
+//     navLink[1].classList.remove('active');
+//     navLink[2].classList.remove('active');
+//     wrapper.classList.remove('active');
+// };
--- shelter/pages/pets/index-pets.js
+++ shelter/pages/pets/index-pets.js
@@ -7,15 +7,48 @@ const navLink = document.querySelectorAll('.nav-link');
 const navLinkPage = document.querySelector('.nav-link.our-pets');
 const navBurger = document.querySelector('.nav-burger');
 const link = document.querySelector('.link');
+const wrapper = document.querySelector('.wrapper')
 
 menu.addEventListener('click', function() {
-    menu.classList.toggle('active')
+    menu.classList.toggle('active');
+    wrapper.classList.toggle('active');
     span.classList.toggle('active');
     navigation.classList.toggle('active');
     navLinkPage.classList.toggle('active');
-    navBurger.classList.toggle('active');
     link.classList.toggle('active');
     navLink[0].classList.toggle('active');
     navLink[2].classList.toggle('active');
     navLink[3].classList.toggle('active');
+    if (navBurger.classList.contains('hidden')){
+        navBurger.classList.remove('hidden');
+    navBurger.classList.toggle('active');
+    }
+    // navBurger.classList.toggle('hidden');
+    navBurger.classList.toggle('active');
+    
+})
+
+navigation.addEventListener('click', function() {
+    menu.classList.toggle('active')
+    span.classList.toggle('active');
+    navigation.classList.toggle('active');
+    navBurger.classList.add('hidden');
+    navLinkPage.classList.toggle('active');
+    link.classList.toggle('active');
+    navLink[0].classList.toggle('active');
+    navLink[2].classList.toggle('active');
+    navLink[3].classList.toggle('active');
+    wrapper.classList.toggle('active');
+})
+wrapper.addEventListener('click', function() {
+    menu.classList.toggle('active')
+    span.classList.toggle('active');
+    navigation.classList.toggle('active');
+    navBurger.classList.add('hidden');
+    navLinkPage.classList.toggle('active');
+    link.classList.toggle('active');
+    navLink[0].classList.toggle('active');
+    navLink[2].classList.toggle('active');
+    navLink[3].classList.toggle('active');
+    wrapper.classList.toggle('active');
 })
